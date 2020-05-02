a7856854d962777fac3e54781e19c6be0ff6720d
Willian Praciano <willian.s.praciano@gmail.com>
2020-05-01 23:08:16 -0300

    creating the basis for the application backend

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+// MongoDB (Banco não relacional)
+mongoose.connect('mongodb+srv://willian:praciano@cluster0-dwzsh.mongodb.net/test?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
